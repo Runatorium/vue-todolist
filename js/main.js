d95913c340ae532c226d7e28d40d3c12c57a9bfe
Runatorium <stefano.alesci1@gmail.com>
2023-03-16 15:40:24 +0100

    markup flags+metodo added

--- js/main.js
+++ js/main.js
@@ -5,15 +5,32 @@ createApp(
     {
         data() {
             return {
-                
-                 
-                    
-            }   
+                todoList: [
+                    {
+                        todo: 'prova1',
+                        done: false,
+                    },
+                    {
+                        todo: 'prova2',
+                        done: true,
+                    },
+                    {
+                        todo: 'prova3',
+                        done: true,
+                    }
+                ]  
+            } 
+            
         },
-        methods: {
-            
-            
-        } 
-          
+        methods:{
+            changeflag(index){
+                if(this.todoList[index].done == false){
+                    this.todoList[index].done = true
+                }else{
+                    this.todoList[index].done = false
+                }
+                
+            }
+        }
     }
 ).mount('#app')
